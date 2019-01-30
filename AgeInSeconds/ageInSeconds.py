e8df6b0de8c917c2cef98990b7e207a15b13234b
Miguel Augusto Tapia <mtapiafdez@gmail.com>
2019-01-30 13:12:33 -0500

    Add files via upload

---
+++ AgeInSeconds/ageInSeconds.py
@@ -0,0 +1,10 @@
+"""
+@author: mtapi
+"""
+
+def age_in_seconds():
+    user_age = input("Enter your age: ")
+    age_seconds = int(user_age) * 365 * 24 * 60 * 60
+    print("You have lived for {} seconds".format(age_seconds))
+    
+age_in_seconds()
